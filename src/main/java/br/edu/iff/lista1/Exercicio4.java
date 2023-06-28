73d59657a0c78bc71bf2fd5db39e4cc5fabb85e4
Daniel Machado <danielaraujoas@gmail.com>
2023-06-28 15:42:27 -0300

    Corrige exercicio4

--- src/main/java/br/edu/iff/lista1/Exercicio4.java
+++ src/main/java/br/edu/iff/lista1/Exercicio4.java
@@ -15,9 +15,7 @@ public class Exercicio4 {
         for (int i = 0; i < vetor.length - 1; i++) {
             for (int j = i + 1; j < vetor.length; j++) {
                 if (vetor[i] < vetor[j]) {
-                    int temp = vetor[i];
-                    vetor[i] = vetor[j];
-                    vetor[j] = temp;
+                    trocarElementos(vetor, i ,j);
                 }
             }
         }
